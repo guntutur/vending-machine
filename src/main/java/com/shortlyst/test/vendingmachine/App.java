8faf925c576de94e7b88fe8dc9690782d4f2f321
Guntur <guntutur@gmail.com>
2019-07-20 22:46:49 +0700

    add main class

---
+++ src/main/java/com/shortlyst/test/vendingmachine/App.java
@@ -0,0 +1,15 @@
+package com.shortlyst.test.vendingmachine;
+
+/**
+ * Created by zer0, the Maverick Hunter
+ * on 20/07/19.
+ * Class: App.java
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        App app = new App();
+        System.out.println("anuan");
+    }
+}
